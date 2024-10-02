0dddf1207cb4ea0f34ce64b94ff71cf54d8e7423
Kaysha OBrien <kayshao@byu.edu>
2024-10-02 17:19:16 -0600

    Start phase 1, implemented newGame functionality

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -24,20 +24,20 @@ public class ChessPiece {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ChessPiece that = (ChessPiece) o;
-        return pieceColor == that.pieceColor && type == that.type;
+        return color == that.color && type == that.type;
     }
 
     @Override
     public String toString() {
         return "ChessPiece{" +
-                "pieceColor=" + pieceColor +
+                "pieceColor=" + color +
                 ", type=" + type +
                 '}';
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(pieceColor, type);
+        return Objects.hash(color, type);
     }
     /**
      * The various different chess piece options
@@ -72,30 +72,41 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+
+
+
+
+
         PieceType piece = board.getPiece(myPosition).getPieceType();
         if (piece == PieceType.KING) {
+            int[][] k_q_directions = {{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
             KingMovesCalculator king = new KingMovesCalculator();
-            return king.pieceMoves(board, myPosition);
+            return king.kingMoves(board, myPosition, k_q_directions);
         }
         if (piece == PieceType.QUEEN) {
+            int[][] k_q_directions = {{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
             QueenMovesCalculator queen = new QueenMovesCalculator();
-            return queen.pieceMoves(board, myPosition);
+            return queen.queenMoves(board, myPosition, k_q_directions);
         }
         if (piece == PieceType.KNIGHT) {
+            int[][] kn_directions = {{1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
             KnightMovesCalculator knight = new KnightMovesCalculator();
-            return knight.pieceMoves(board, myPosition);
+            return knight.knightMoves(board, myPosition, kn_directions);
         }
         else if (piece == PieceType.BISHOP) {
+            int[][] b_directions = {{1,1}, {1, -1}, {-1, -1}, {-1, 1}};
             BishopMovesCalculator bishop = new BishopMovesCalculator();
-            return bishop.pieceMoves(board, myPosition);
+            return bishop.bishopMoves(board, myPosition, b_directions);
         }
         else if (piece == PieceType.ROOK) {
+            int[][] r_directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
             RookMovesCalculator rook = new RookMovesCalculator();
-            return rook.pieceMoves(board, myPosition);
+            return rook.rookMoves(board, myPosition, r_directions);
         }
         else if (piece == PieceType.PAWN) {
+            int[][] p_directions = {{1,0}, {-1,0}};
             PawnMovesCalculator pawn = new PawnMovesCalculator();
-            return pawn.pieceMoves(board, myPosition);
+            return pawn.pawnMoves(board, myPosition, p_directions);
         }
         else return null;
     }
