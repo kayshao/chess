ba0f0f26af9c93209c5207997b5dbee9af5cf132
Kaysha OBrien <kayshao@byu.edu>
2024-11-21 15:22:23 -0700

    implemented postlogin help

--- client/src/main/java/ui/PostLoginUI.java
+++ client/src/main/java/ui/PostLoginUI.java
@@ -1,13 +1,47 @@
 package ui;
+import result.*;
 
-import java.util.Scanner;
+import java.util.Arrays;
+
+import static ui.EscapeSequences.*;
 
 public class PostLoginUI {
-    public ServerFacade facade;
+    private final ServerFacade facade;
 
-    public PostLoginUI(ServerFacade server) {
-        facade = server;
+    public PostLoginUI(ServerFacade facade) {
+        this.facade = facade;
     }
-
-    public void run() {}
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                // case "logout" -> logout(params);
+                // case "new" -> createGame(params);
+                // case "list" -> listGames(params);
+                // case "play" -> playGame(params);
+                // case "observe" -> observeGame(params);
+                default -> help();
+            };
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+    public String help() {
+        return SET_TEXT_COLOR_YELLOW + """
+                Chess user help page
+                type a command to get started
+                """ +
+                SET_TEXT_COLOR_WHITE + "to create a game" +
+                SET_TEXT_COLOR_LIGHT_GREY + " - new <game_name>\n" +
+                SET_TEXT_COLOR_WHITE + "to see a list of games" +
+                SET_TEXT_COLOR_LIGHT_GREY + " - list\n" +
+                SET_TEXT_COLOR_WHITE + "to play a game" +
+                SET_TEXT_COLOR_LIGHT_GREY + " - play <game_number> <color>\n" +
+                SET_TEXT_COLOR_WHITE + "to observe a game" +
+                SET_TEXT_COLOR_LIGHT_GREY + " - observe <game_number>\n" +
+                SET_TEXT_COLOR_WHITE + "to log out" +
+                SET_TEXT_COLOR_LIGHT_GREY + " - logout";
+    }
 }
