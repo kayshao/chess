2c88c0402f39e1797d1eeb99cde523d28c9661f4
Kaysha OBrien <kayshao@byu.edu>
2024-11-22 16:36:08 -0700

    fixed prelogin ui errors

--- client/src/main/java/ui/PreLoginUI.java
+++ client/src/main/java/ui/PreLoginUI.java
@@ -25,24 +25,25 @@ public class PreLoginUI {
             default -> help();
         };
         } catch (Exception e) {
-            return e.getMessage();
+            return "Error, type 'help for help";
         }
 
     }
     public String register(String... params) throws Exception {
-        if (params.length >= 3) {
+        if (params.length == 3) {
             RegisterResult result = facade.register(params[0], params[1], params[2]);
-            return String.format("Account created, username: %s", result.username());
+            System.out.println("Account created");
+            return "a " + result.authToken();
         }
         throw new Exception("Invalid registration, expected <username> <password> <email>");
     }
     public String logIn(String... params) throws Exception {
-        if (params.length >= 2) {
+        if (params.length == 2) {
             LoginResult result = facade.login(params[0], params[1]);
             System.out.println(SET_TEXT_COLOR_BLUE + "Successful sign in\n");
             return "a " + result.authToken();
         }
-        throw new Exception("Invalid login");
+        throw new Exception("Invalid login, expected <username> <password>");
     }
     public String help() {
         return SET_TEXT_COLOR_YELLOW + """
@@ -53,6 +54,8 @@ public class PreLoginUI {
                 SET_TEXT_COLOR_WHITE + "register <username> <password> <email>\n" +
                 SET_TEXT_COLOR_LIGHT_GREY + "to log in - type " +
                 SET_TEXT_COLOR_WHITE + "login <username> <password>\n" +
+                SET_TEXT_COLOR_LIGHT_GREY + "to show help menu - type " +
+                SET_TEXT_COLOR_WHITE + "help\n" +
                 SET_TEXT_COLOR_LIGHT_GREY + "to quit - type " +
                 SET_TEXT_COLOR_WHITE + "quit\n";
     }
